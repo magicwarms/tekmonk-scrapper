48d12ba72866d53e09f1d99bfab96092f553176b
Andhana Utama <magicwarms@gmail.com>
2022-06-30 17:09:30 +0700

    fix(fix bug and refactor into promise): refactor into promise and use my own code

--- routes/routes.js
+++ routes/routes.js
@@ -2,119 +2,80 @@ const http = require('http');
 const https = require('https');
 const { responseSuccess, responseError } = require('../apps/utilities/utilities');
 
-const getTimeStories = async (cb) => {
-    // return new Promise((resolve, reject) => {
-    //     https
-    //         .get('https://time.com/', (response) => {
-    //             const data = [];
-    //             response.on('data', (chunkData) => data.push(chunkData));
-    //             response.on('end', () => {
-    //                 // const rawResult = data.join('');
-    //                 const rawResult = data.toString('utf8');
+const getTimeStories = async () => {
+    return new Promise((resolve, reject) => {
+        https
+            .get('https://time.com/', (response) => {
+                const data = [];
+                response.on('data', (chunkData) => data.push(chunkData));
+                response.on('end', () => {
+                    const getLines = data.toString('utf8').split('\n');
+                    const arrayOfStories = getLines
+                        .filter((line) => line.includes('latest-stories__item') || line.includes('a href='))
+                        .slice(40, 64);
 
-    //                 const getLines = rawResult.split('\n');
-    //                 const arrayOfStories = getLines
-    //                     .filter((line) => line.includes('latest-stories__item') || line.includes('a href='))
-    //                     .slice(40, 62);
-    //                 const getNecessaryLines = [];
-    //                 let itemTitle = '';
-    //                 let itemLink = '';
-    //                 arrayOfStories.forEach((item) => {
-    //                     if (item.includes('a href=')) {
-    //                         itemLink = item.replace(/<a href="/g, '').replace(/">/g, '');
-    //                     }
-    //                     if (item.match(/<h3 class="latest-stories__item-headline">(.*?)<\/h3>/g)) {
-    //                         itemTitle = item
-    //                             .replace(/<h3 class="latest-stories__item-headline">/g, '')
-    //                             .replace(/<\/h3>/g, '');
-    //                     }
-    //                     getNecessaryLines.push({ title: itemTitle.trim(), link: `https://time.com${itemLink.trim()}` });
-    //                 });
-    //                 // mapping the link property
-    //                 const links = getNecessaryLines.map((o) => o.link);
-    //                 // filter to delete unique data
-    //                 resultFiltered = getNecessaryLines.filter(({ link }, index) => !links.includes(link, index + 1));
-    //                 // return the result
-    //                 resolve(resultFiltered);
-    //             });
-    //         })
-    //         .on('error', (e) => {
-    //             console.error('error', e.message);
-    //             reject(e);
-    //         })
-    //         .end();
-    // });
-    https
-        .get('https://time.com/', (response) => {
-            const data = [];
-            response.on('data', (chunkData) => data.push(chunkData));
-
-            response.on('end', () => {
-                // const rawResult = data.join('');
-                const rawResult = data.toString('utf8');
-
-                const getLines = rawResult.split('\n');
-                const arrayOfStories = getLines
-                    .filter((line) => line.includes('latest-stories__item') || line.includes('a href='))
-                    .slice(40, 62);
-                const getNecessaryLines = [];
-                let itemTitle = '';
-                let itemLink = '';
-                arrayOfStories.forEach((item) => {
-                    if (item.includes('a href=')) {
-                        itemLink = item.replace(/<a href="/g, '').replace(/">/g, '');
-                    }
-                    if (item.match(/<h3 class="latest-stories__item-headline">(.*?)<\/h3>/g)) {
-                        itemTitle = item
-                            .replace(/<h3 class="latest-stories__item-headline">/g, '')
-                            .replace(/<\/h3>/g, '');
-                    }
-                    getNecessaryLines.push({ title: itemTitle.trim(), link: `https://time.com${itemLink.trim()}` });
+                    const getNecessaryLines = [];
+                    let itemTitle = '';
+                    let itemLink = '';
+                    arrayOfStories.forEach((item) => {
+                        if (item.match('a href=')) {
+                            itemLink = item
+                                .replace(/<a href="/g, '')
+                                .replace(/">/g, '')
+                                .trim();
+                        }
+                        if (item.match(/latest-stories__item-headline/g)) {
+                            itemTitle = item
+                                .replace(/<h3 class="latest-stories__item-headline">/g, '')
+                                .replace(/<\/h3>/g, '')
+                                .trim();
+                        }
+                        if (itemTitle !== '') {
+                            getNecessaryLines.push({
+                                title: itemTitle,
+                                link: `https://time.com${itemLink}`,
+                            });
+                        }
+                    });
+                    // mapping the link property
+                    const links = getNecessaryLines.map((o) => o.link);
+                    // filter to delete duplicate data
+                    resultFiltered = getNecessaryLines.filter(({ link }, index) => !links.includes(link, index + 1));
+                    resolve(resultFiltered);
                 });
-                // mapping the link property
-                const links = getNecessaryLines.map((o) => o.link);
-                // filter to delete unique data
-                resultFiltered = getNecessaryLines.filter(({ link }, index) => !links.includes(link, index + 1));
-                // return the result
-                cb(resultFiltered);
-            });
-        })
-        .on('error', (e) => {
-            cb(e);
-            console.error('error', e.message);
-        });
+            })
+            .on('error', (e) => {
+                console.error('error', e.message);
+                reject(e);
+            })
+            .end();
+    });
 };
 
 module.exports = http.createServer(async (req, res) => {
     // set the request route
     if (req.url === '/getTimeStories' && req.method === 'GET') {
-        getTimeStories((result) => {
-            if (result.code === 'ENOTFOUND') {
-                res.writeHead(404, { 'Content-Type': 'application/json' });
+        getTimeStories()
+            .then((getData) => {
+                // response headers
+                res.writeHead(200, { 'Content-Type': 'application/json' });
+                // end the response and return it
+                return res.end(responseSuccess(getData));
+            })
+            .catch((err) => {
+                if (err.code === 'ENOTFOUND') {
+                    res.writeHead(404, { 'Content-Type': 'application/json' });
+                    // send the error
+                    return res.end(responseError(err.message));
+                }
+                res.writeHead(500, { 'Content-Type': 'application/json' });
                 // send the error
-                return res.end(responseError(result.message));
-            }
-            console.log({ result });
-            // response headers
-            res.writeHead(200, { 'Content-Type': 'application/json' });
-            // end the response and return it
-            return res.end(responseSuccess(result));
-        });
-        // try {
-        //     const getData = await getTimeStories();
-        //     // response headers
-        //     res.writeHead(200, { 'Content-Type': 'application/json' });
-        //     // end the response and return it
-        //     return res.end(responseSuccess(getData));
-        // } catch (err) {
-        //     if (err.code === 'ENOTFOUND') {
-        //         res.writeHead(404, { 'Content-Type': 'application/json' });
-        //         // send the error
-        //         return res.end(responseError(err.message));
-        //     }
-        //     res.writeHead(500, { 'Content-Type': 'application/json' });
-        //     // send the error
-        //     return res.end(responseError(err.message));
-        // }
+                return res.end(responseError(err.message));
+            });
+    } else {
+        // response headers
+        res.writeHead(404, { 'Content-Type': 'application/json' });
+        // end the response and return it
+        return res.end(responseSuccess({ message: 'API Route not found' }));
     }
 });
